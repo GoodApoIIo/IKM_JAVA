37591d7dd0c37f5d78d7a6cca6a06a32e5997249
Ekaterina <148027158+GoodApoIIo@users.noreply.github.com>
2025-01-14 10:38:03 +0500

    Add files via upload

--- IKM/src/main/java/spring/database/repositories/CountryRepository.java
+++ IKM/src/main/java/spring/database/repositories/CountryRepository.java
@@ -1,8 +1,7 @@
 package spring.database.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import spring.database.models.entity.Country;
 
-public interface CountryRepository extends JpaRepository<Country, Integer>, JpaSpecificationExecutor<Country>{
+public interface CountryRepository extends JpaRepository<Country, Integer> {
 }
